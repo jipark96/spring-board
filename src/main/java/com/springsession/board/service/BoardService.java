ec9a350cd16a3ebe9c6bdf796d08909f6c67ab89
jipark96 <wlgus996@naver.com>
2022-11-14 22:58:09 +0900

    crud 2주차

--- src/main/java/com/springsession/board/service/BoardService.java
+++ src/main/java/com/springsession/board/service/BoardService.java
@@ -5,6 +5,8 @@ import com.springsession.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BoardService {
 
@@ -14,8 +16,19 @@ public class BoardService {
         this.boardRepository = boardRepository;
     }
 
+    //글 작성
     public void write(Board board) {
 
         boardRepository.save(board);
+    }
+
+    //게시물 리스트 처리
+    public List<Board> boardList() {
+        return boardRepository.findAll();
+    }
+
+    //특정 게시물 불러오기
+    public Board boardView(Integer id) {
+        return boardRepository.findById(id).get();
     }
 }
